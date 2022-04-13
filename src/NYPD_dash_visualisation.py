a9ba1a35f142308f958b67793948cb8c0cb55de1
Moustapha DIOP <moustapha.diop@epita.fr>
2022-04-13 10:12:22 +0200

    (add) new ui

--- src/NYPD_dash_visualisation.py
+++ src/NYPD_dash_visualisation.py
@@ -10,6 +10,12 @@ from figures.scatter_figure import display_correlation_scatter
 from figures.types_figure import types_of_calls
 from figures.type_inout_temp_figure import in_out_of_calls
 
+from helpers.design import background_color, font_color, font_family
+
+from helpers.utils import load_weather_data
+
+weather_data = load_weather_data()
+
 app = Dash(__name__)
 
 
@@ -20,106 +26,212 @@ app = Dash(__name__)
     Input("date-picker-range", "end_date"),
 )
 def correlation_figure(freq, start, end):
+    freq = freq or "Month"
     return display_correlation_plot(freq[0], start=start, end=end)
 
 
 @app.callback(
     Output("scatter", "figure"),
-    Input("freq", "value"),
-    Input("date-picker-range", "start_date"),
-    Input("date-picker-range", "end_date"),
+    Input("freq-s", "value"),
+    Input("date-picker-range-s", "start_date"),
+    Input("date-picker-range-s", "end_date"),
 )
 def scatter_figure(freq, start, end):
+    freq = freq or "Month"
     return display_correlation_scatter(freq[0], start=start, end=end)
 
 
-@app.callback(
-    Output("types", "figure"),
-    Input("freq", "value"),
-    Input("date-picker-range", "start_date"),
-    Input("date-picker-range", "end_date"),
-)
-def types_figure(freq, start, end):
-    return types_of_calls(freq[0], start=start, end=end)
+# @app.callback(
+#     Output("types", "figure"),
+#     Input("freq", "value"),
+#     Input("date-picker-range", "start_date"),
+#     Input("date-picker-range", "end_date"),
+# )
+# def types_figure(freq, start, end):
+#     return types_of_calls(freq[0], start=start, end=end)
 
 
-@app.callback(
-    Output("types_in_out", "figure"),
-    Input("freq", "value"),
-    Input("date-picker-range", "start_date"),
-    Input("date-picker-range", "end_date"),
-)
-def types_in_out_figure(freq, start, end):
-    return in_out_of_calls(freq[0], start=start, end=end)
+# @app.callback(
+#     Output("types_in_out", "figure"),
+#     Input("freq", "value"),
+#     Input("date-picker-range", "start_date"),
+#     Input("date-picker-range", "end_date"),
+# )
+# def types_in_out_figure(freq, start, end):
+#     return in_out_of_calls(freq[0], start=start, end=end)
 
+
+paraf = """
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+"""
+
+parafscatter = """
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+"""
 
 app.layout = html.Div(
     [
-        html.Div(
-            [
-                dcc.DatePickerRange(
-                    id="date-picker-range",
-                    min_date_allowed=date(2018, 1, 1),
-                    max_date_allowed=date(2020, 12, 31),
-                    start_date=date(2018, 1, 1),
-                    end_date=date(2020, 12, 31),
-                ),
-                dcc.Dropdown(
-                    id="freq", options=["Day", "Week", "Month"], value="Month"
-                ),
-            ],
+        html.H1("NYPD Calls en fonction de la météo à New York",
             style={
-                "width": "100%",
-                "margin-left": "10px",
-                "display": "inline-block",
-                "vertical-align": "top",
-                "height": "20%",
-            },
+                "text-align": "center",
+                "margin-bottom": "80px",
+                "font-size": "50px",
+                'font-family': font_family,
+                'font-color': font_color,
+            }),
+
+        html.Div([
+            html.Div(
+                [
+                    dcc.DatePickerRange(
+                        id="date-picker-range",
+                        min_date_allowed=date(2018, 1, 1),
+                        max_date_allowed=date(2020, 12, 31),
+                        start_date=date(2018, 1, 1),
+                        end_date=date(2020, 12, 31),
+                        style={
+                            "background-color": background_color,
+                            "font-color": font_color,
+                            "font-family": font_family,
+                            "margin-bottom": "5%",
+                        }
+                    ),
+                    dcc.Dropdown(
+                        id="freq",
+                        options=["Day", "Week", "Month"],
+                        value="Month",
+                        style={
+                            "background-color": background_color,
+                            "font-color": font_color,
+                            "font-family": font_family,
+                        }
+                    ),
+                ],
+                style={
+                    "width": "22%",
+                    "margin-right": "20px",
+                    "display": "inline-block",
+                    "vertical-align": "top",
+                    "margin-top": "10%" 
+                },
+            ),
+
+            html.Div(
+                [
+                    dcc.Graph(id="correlation"),
+                ],
+                style={
+                    "width": "75%",
+                    "display": "inline-block",
+                    "vertical-align": "top",
+                },
+            ),
+
+            html.P(paraf, style={"text-align": "justify", "margin-right": "15%", "margin-left": "15%"})
+
+            ],style={
+            'justifyContent':'center',
+            'text-align': 'center',
+            "font-size": "20px",
+            "margin-bottom": "90px"}
         ),
-        html.Div(
-            [
-                dcc.Graph(id="correlation"),
-            ],
-            style={
-                "width": "100%",
-                "display": "inline-block",
-                "vertical-align": "top",
-                "height": "100%",
-            },
-        ),
-        html.Div(
-            [
-                dcc.Graph(id="scatter"),
-            ],
-            style={
-                "width": "100%",
-                "display": "inline-block",
-                "vertical-align": "bottom",
-            },
-        ),
-        html.Div(
-            [
-                dcc.Graph(id="types"),
-            ],
-            style={
-                "width": "70%",
-                "display": "inline-block",
-                "vertical-align": "bottom",
-            },
-        ),
-        html.Div(
-            [
-                dcc.Graph(id="types_in_out"),
-            ],
-            style={
-                "width": "40%",
-                "display": "inline-block",
-                "vertical-align": "bottom",
-            },
-        ),
+
+        html.Div([
+            html.Div(
+                [
+                    dcc.Graph(id="scatter"),
+                ],
+                style={
+                    "width": "75%",
+                    "display": "inline-block",
+                    "vertical-align": "top",
+                },
+            ),
+
+            html.Div(
+                [
+                    dcc.DatePickerRange(
+                        id="date-picker-range-s",
+                        min_date_allowed=date(2018, 1, 1),
+                        max_date_allowed=date(2020, 12, 31),
+                        start_date=date(2018, 1, 1),
+                        end_date=date(2020, 12, 31),
+                        style={
+                            "background-color": background_color,
+                            "font-color": font_color,
+                            "font-family": font_family,
+                            "margin-bottom": "5%",
+                        }
+                    ),
+                    dcc.Dropdown(
+                        id="freq-s",
+                        options=["Day", "Week", "Month"],
+                        value="Month",
+                        style={
+                            "background-color": background_color,
+                            "font-color": font_color,
+                            "font-family": font_family,
+                        }
+                    ),
+                ],
+                style={
+                    "width": "22%",
+                    "margin-right": "30px",
+                    "display": "inline-block",
+                    "vertical-align": "top",
+                    "margin-top": "10%" 
+                },
+            ),
+
+            html.P(parafscatter, style={"text-align": "justify", "margin-right": "15%", "margin-left": "15%"})
+
+        ], style={
+            'justifyContent':'center',
+            'text-align': 'center',
+            "font-size": "20px",
+            "margin-bottom": "90ppx"
+        }),
+
+        
+        #html.Div(
+        #    [
+        #        dcc.Graph(id="types"),
+        #    ],
+        #   style={
+        #        "width": "70%",
+        #        "display": "inline-block",
+        #        "vertical-align": "bottom",
+        #    },
+        #),
+        #html.Div(
+        #    [
+        #        dcc.Graph(id="types_in_out"),
+        #    ],
+        #    style={
+        #        "width": "40%",
+        #        "display": "inline-block",
+        #        "vertical-align": "bottom",
+        #    },
+        #),
     ],
-    style={"width": "70%", "padding-left": "15%"},
+    style={
+        "padding": "4%",
+        "background-color": background_color,
+        "font-color": font_color,
+        "font-family": font_family
+        },
 )
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
--- src/figures/scatter_figure.py
+++ src/figures/scatter_figure.py
@@ -3,6 +3,9 @@ from helpers.utils import (
     load_weather_data,
     remove_outliers,
 )
+
+from helpers.design import background_color, font_color, font_family, color_blue, color_green
+
 import plotly.express as px
 
 calls = load_calls_correlation_data()
@@ -21,11 +24,21 @@ def display_correlation_scatter(freq="W", start=None, end=None):
     wspd = weather_data.prcp.resample(freq).mean()
     wspd = remove_outliers(wspd)
 
-    fig = px.scatter(x=tavg, y=nb_calls, trendline="ols", size=wspd)
+    fig = px.scatter(x=tavg, y=nb_calls, trendline="ols", size=wspd,
+        color_discrete_sequence=[color_blue], trendline_color_override=color_green)
+
     fig.update_xaxes(title_text="Temperature moyenne")
     fig.update_yaxes(title_text="Nombre d'appels")
+    
     fig.update_layout(
+        title_text=f"Nombre d'appels en fonction de la température moyenne",
+        title_x=0.5,
         margin=dict(l=10, r=10, b=10, t=50, pad=4),
+        plot_bgcolor=background_color,
+        paper_bgcolor=background_color,
+        autosize=True,
+        font_family=font_family,
+        font_color=font_color,
     )
 
     return fig
